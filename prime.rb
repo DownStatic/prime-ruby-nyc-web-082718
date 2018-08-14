b50355f98415cf819e21b0c0ac50659bbc171e21
Christopher Stull <chris.stull@gmail.com>
2018-08-14 17:52:33 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,10 @@
 # Add  code here!
 def prime?(int)
+  if int%2==0
+    return true
+  end
   for div in (2..int)
-      if int%div==0 and int!=div
+      if int%div==0
         return true
       end
     end
