92bb77dcae2717b23bd51cdd6d7d84da3b151424
Christopher Stull <chris.stull@gmail.com>
2018-08-14 17:59:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(int)
+  if int < 0
+    int = -1*int
+  end
+  
+  if int == 1 or int==0
+    return false
+  end
+  
+  for div in (2..int)
+      if int%div==0 and div != int
+        return false
+      end
+    end
+  return true
+end
