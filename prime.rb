0351d677567f46d1a0a43dee79d5de137215f0f3
Christopher Stull <chris.stull@gmail.com>
2018-08-14 17:44:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,10 @@
 # Add  code here!
 def prime?(int)
   if int%2==0
-    for div in (0..int/2)
-      if int/div==0
         retun true
-      end
     end
   else
-    for div in (0..(int/2 - 0.5))
+    for div in (3..(int/2 - 0.5))
       if int/div==0
         return true
       end
