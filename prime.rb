209c7fd9ef085e2b8d34fbbf2cf39b817e32b454
Christopher Stull <chris.stull@gmail.com>
2018-08-14 17:59:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,11 +4,7 @@ def prime?(int)
     int = -1*int
   end
   
-  if int%2==0 and int!=0 and int!=1
-    return true
-  end
-  
-  if int == 1
+  if int == 1 or int==2 or int==0
     return false
   end
   
